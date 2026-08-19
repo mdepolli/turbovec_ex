80191ff998d1faa5ca3caa0467bf464107b735e9
Marcelo De Polli <marcelo.polli@gmail.com>
2026-08-18 22:52:43 -0300

    Add initial mix project

---
+++ lib/turbovec.ex
@@ -0,0 +1,18 @@
+defmodule TurboVec do
+  @moduledoc """
+  Documentation for `TurboVec`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TurboVec.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/turbovec_test.exs
@@ -0,0 +1,8 @@
+defmodule TurboVecTest do
+  use ExUnit.Case
+  doctest TurboVec
+
+  test "greets the world" do
+    assert TurboVec.hello() == :world
+  end
+end
