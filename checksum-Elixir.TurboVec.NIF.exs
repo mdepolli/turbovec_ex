d551e2a2af116fcb813f60e8234eae3efa655060
Marcelo De Polli <marcelo.polli@gmail.com>
2026-08-20 03:08:07 -0300

    Add precompiled NIF checksums for 0.2.0

--- checksum-Elixir.TurboVec.NIF.exs
+++ checksum-Elixir.TurboVec.NIF.exs
@@ -1,9 +1,9 @@
 %{
-  "libturbovec_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:62acaa96d1c006bc8026a0d1498c208e37d4cc559118b37df72ce72c2166f4f5",
-  "libturbovec_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:fed06f0da70727b2433c9af4d1220141bbe438d6c220480bea8667fe3f6caca8",
-  "libturbovec_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:60670b7473f1ffe028c6c1c17ecde58d5eadfb0aedb846fd4de2a8f61aab4f71",
-  "libturbovec_nif-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:e7e311bd3b398cf1e8b44b1ffa9341ad8ab7a0066863e9133e1efccfb89aa30b",
-  "libturbovec_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:841b87f296accecded594d6d4c19f05f585d3e7fd27f730f3f7afa8b6e39cd36",
-  "libturbovec_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:0be1800981e550a15c2980dcb0b4daca9bebfc97b12e3b4b2067fd4961f25945",
-  "turbovec_nif-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:e1b8feeca387c34c9c37389ab643fab5fb29dd1c1dfde7260353e94b87c64ee4",
+  "libturbovec_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:f7452676d3998a96b639fe610fd45bf6ad7c71f6fa1a2b75a46cd93b6ac7bd96",
+  "libturbovec_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7b7f0d0024b1d80aae50382e4119b99cace0cd4a3bbf2e318f5510c2078c4bd4",
+  "libturbovec_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:df43ce427a325436855aaccb8c62cbfbaeec5432dc1f3d49bbb8c968663302e9",
+  "libturbovec_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:0e15b6d5e81611bd5914b7e2f0ccf4fcefc1eab633ae8486cd23259cc0945cc1",
+  "libturbovec_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:cfe4390ada735fdd079b102cbb11c4e55e8661001dd62c1246f68c9c87500e30",
+  "libturbovec_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:6aaf8ab6cf2ad7abafc75f7011f42f7df22fe751f0e7763a9c2add83e1feb579",
+  "turbovec_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:8e523967cfd2558b4123d37774d0f076c4f1f506ee6dc391e3459f04888d3334",
 }
